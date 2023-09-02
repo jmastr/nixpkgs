1fb6a779dd0c0158916b4a6c53a9638dd9524ee4
Mostly Void <7rat13@gmail.com>
2023-09-02 12:37:29 +0200

    cronopete: init at 4.16.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3088,6 +3088,8 @@ with pkgs;
 
   cron = isc-cron;
 
+  cronopete = callPackage ../tools/backup/cronopete { };
+
   cudaPackages_10_0 = callPackage ./cuda-packages.nix { cudaVersion = "10.0"; };
   cudaPackages_10_1 = callPackage ./cuda-packages.nix { cudaVersion = "10.1"; };
   cudaPackages_10_2 = callPackage ./cuda-packages.nix { cudaVersion = "10.2"; };
